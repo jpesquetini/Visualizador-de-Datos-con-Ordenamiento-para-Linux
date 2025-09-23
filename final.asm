0a500d498c94d911989a8d0b06963f7188aaa081
jpesquetinif <jpesquetinif@gmail.com>
2025-09-22 23:46:57 -0600

    listo, falta corregir la barra y afinar el bubble sort

--- final.asm
+++ final.asm
@@ -13,11 +13,12 @@ section .bss
 	caracter_barra resb 3
 	color_barra resb 3
 	color_fondo resb 3
-	format resb 8
+	format resb 32
 	inventario resb 512
 	bytes_inventario resq 1
 	inventario_pointers resb 64
 	msg resb 512
+	barra resb 64
 
 section .text
         global _start
@@ -143,7 +144,7 @@ cmp_color_fondo:
 	and rax, r12
 	mov [color_fondo], rax
 	;print color_fondo
-	jmp print_hello
+	jmp cargar_formato
 
 mascara:
 	push rbp
@@ -163,7 +164,7 @@ end_mascara:
 	pop rbp
 	ret
 
-print_hello:
+cargar_formato:
 	mov bl, 27
 	mov [format], bl
 	mov bl, 91
@@ -176,10 +177,8 @@ print_hello:
 	mov [format+5], bx
 	mov bl, 109
 	mov [format+7], bl
-	print format
-	print hello
 
-lectura_invnentario:
+lectura_inventario:
 	; Open the file
         mov rax, SYS_OPEN
         mov rdi, inventario_path
@@ -256,8 +255,100 @@ bubble_i:
 	mov r10, 4
 	mov r12d, [inventario_pointers+r8]
 	cmp r12d, 0
-	je end_bubble
+	mov r15, 0
+	je imprimir_inventario
 	jmp bubble_compare
+
+imprimir_inventario:	
+	; Imprimir nombre del inventario
+	mov r8d, [inventario_pointers+r15]
+	cmp r8d, 0
+	je exit_script
+	mov rsi, r8
+	mov rdi, msg
+	call copiar_nombre
+	print msg
+	mov r10, 0
+	call limpiar_msg
+
+	; Imprimir formato de colores
+	print format
+	
+	; Imprimir barra
+	mov rsi, r8
+	mov rdi, barra
+	call cargar_barra
+	mov r13, [barra]
+	call imprimir_barra
+
+	; Devolver colores a lo normal
+	print reset_color
+
+	; Imprimir cantidad
+	print barra
+
+	add r15, 4
+	jmp imprimir_inventario
+
+copiar_nombre:
+	mov al, [rsi]
+	mov [rdi], al
+	inc rsi
+	inc rdi
+	cmp al, 58
+	jnz copiar_nombre
+	ret
+
+limpiar_msg:
+	mov al, [msg+r10]
+	cmp al, 0
+	je terminar_limpieza
+	mov al, 0
+	mov [msg+r10], al
+	inc r10
+	jmp limpiar_msg
+
+terminar_limpieza:
+	ret
+
+cargar_barra:
+	mov al, [rsi]
+	inc rsi
+	cmp al, 58
+	jnz cargar_barra
+
+guardar_valor:
+	mov al, [rsi]
+	mov [rdi], al
+	inc rsi
+	inc rdi
+
+guardar_otros_valores:
+	mov al, [rsi]
+	cmp al, 10
+	je regreso_inventario
+	jmp guardar_valor
+			
+regreso_inventario:
+	mov [rdi], al
+	ret
+	
+imprimir_barra:
+	mov al, r13b
+	sub al, 48
+
+imprimir_caracter:
+	print caracter_barra
+	sub al, 1
+	cmp al, 0
+	jnz imprimir_caracter
+	shr r13, 8
+	cmp r13, 0
+	je regreso_caracter
+	jmp imprimir_barra
+
+regreso_caracter:
+	ret
 
 end_bubble:
 	mov r8d, [inventario_pointers+r9]
